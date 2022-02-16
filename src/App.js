b8427ccf73f41f5da18b324d8754339bb21936ac
colefiscus <coletfiscus@gmail.com>
2022-02-16 13:39:14 -0700

    Add ideas to initial state in App

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ class App extends Component {
         {id: 1, title: "Plan nighttime routine", description: "Write out series of 5 or more tasks to complete every night to signify it's time for bed."},
         {id: 2, title: "Import Spotify playlists", description: "Go to TIDAL's website to start the import process of Spotify playlists to TIDAL."},
         {id: 3, title: "Make 30 minute work mode", description: "Figure out way to mmore consistently stand up at desk, take breaks, and increase productivity."},
+        {id: 4, title: "Make biscuits", description: "Find a recipe for biscuits and then make them."},
+        {id: 5, title: "Paint a picture", description: "Buy some painting things and then use them on a wall."},
+        {id: 6, title: "Do yoga", description: "Create a yoga routine for cats, figure out how to train them."},
+        {id: 7, title: "Go for a walk", description: "Create leash that can control 20 dogs at once."},
+        {id: 8, title: "Sculpt a pot", description: "Invent method that makes a perfect pot every time."},
+        {id: 9, title: "Do a workout", description: "Burn 700 calories in 15 minutes of exercise."},
       ],
     }
   }
