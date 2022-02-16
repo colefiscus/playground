18b1068e383ef33bcc033fc2176246810c588992
colefiscus <coletfiscus@gmail.com>
2022-02-15 18:18:59 -0700

    Add initial state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      ideas: [],
+      ideas: [
+        {id: 1, title: "Plan nighttime routine", description: "Write out series of 5 or more tasks to complete every night to signify it's time for bed."},
+        {id: 2, title: "Import Spotify playlists", description: "Go to TIDAL's website to start the import process of Spotify playlists to TIDAL."},
+        {id: 3, title: "Make 30 minute work mode", description: "Figure out way to mmore consistently stand up at desk, take breaks, and increase productivity."},
+      ],
     }
   }
 
