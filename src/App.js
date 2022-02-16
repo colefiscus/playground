55bf8e18e53802773b96641c5566d15dc8ffb288
colefiscus <coletfiscus@gmail.com>
2022-02-15 18:15:12 -0700

    Create App component, rework render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React, { Component } from "react";
 import GlobalStyles from "./GlobalStyles";
 
-function App() {
-  return (
-    <div className="App">
-      <GlobalStyles />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: [],
+    }
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <GlobalStyles />
+        <h1>IDEABOX</h1>
+        <p>Hello there!</p>
+      </main>
+    );
+  }
 }
 
 export default App;
