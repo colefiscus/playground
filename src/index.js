ef7f038269dd73f26b3297f0a6f3dcf0d25f4573
colefiscus <coletfiscus@gmail.com>
2022-02-15 17:52:15 -0700

    Remove boilerplate and CSS files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
+
+// Use -- import styled from 'styled-components/macro' -- for all styled-components imports
