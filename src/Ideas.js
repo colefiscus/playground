997537259f4af9a923463453d6f36f1a05ea74e4
colefiscus <coletfiscus@gmail.com>
2022-02-16 14:05:20 -0700

    Add comment for grid-auto-flow

--- src/Ideas.js
+++ src/Ideas.js
@@ -27,8 +27,10 @@ const Ideas = ({ ideas, deleteIdea }) => {
 const IdeasContainer = styled.section`
   padding: 20px;
   display: grid;
-  /* grid-template-columns: 300px 300px 100px; Pixel-based columns */
-  grid-template-columns: 20% 50% 30%;
+  /* grid-auto-flow: column; ~~ Add column-based flow ~~ */
+  /* grid-template-columns: 300px 300px 300px; ~~ Pixel-based columns ~~ */
+  /* grid-template-columns: 20% 50% 30%; ~~ Percentage-based columns ~~ */
+  
   grid-gap: 20px;
 `;
 
