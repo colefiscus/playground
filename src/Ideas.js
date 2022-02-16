34249d0b6f788ef3edf26babfede60942c397ce7
colefiscus <coletfiscus@gmail.com>
2022-02-15 20:58:13 -0700

    Import Card to Ideas and add to return

--- src/Ideas.js
+++ src/Ideas.js
@@ -1,9 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
+import Card from "./Card";
+
 const Ideas = ({ ideas }) => {
   return (
-    <h2>Ideas Go Here!</h2>
+    <div className="ideasContainer">
+      <h2>Ideas Go Here!</h2>
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
 
