0e9903687bbe8cd71bc68d6e4a12609f5700df01
colefiscus <coletfiscus@gmail.com>
2022-02-17 22:35:37 -0700

    Change columns to auto-fill on grid

--- src/Ideas.js
+++ src/Ideas.js
@@ -30,7 +30,7 @@ const IdeasContainer = styled.section`
   /* grid-auto-flow: column; ~~ Add column-based flow ~~ */
   justify-content: center;
   align-items: end;
-  grid-template-columns: repeat(3, 200px);
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   /* grid-template-columns: 300px 300px 300px; ~~ Pixel-based columns ~~ */
   /* grid-template-columns: 20% 50% 30%; ~~ Percentage-based columns ~~ */
   gap: 20px 40px;
