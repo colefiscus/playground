37b89bc2ff2b04c2028d877d2ff12dced4a20ee6
colefiscus <coletfiscus@gmail.com>
2022-02-18 20:32:02 -0700

    Add div to alter Card scroll bar appearance

--- src/Card.js
+++ src/Card.js
@@ -3,21 +3,28 @@ import styled from "styled-components/macro";
 
 const Card = ({ id, title, description, deleteIdea }) => {
   return (
-    <CardWrapper>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <button onClick={() => deleteIdea(id)}>🗑</button>
-    </CardWrapper>
+    <BigCardWrapper>
+      <CardWrapper>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <button onClick={() => deleteIdea(id)}>🗑</button>
+      </CardWrapper>
+    </BigCardWrapper>
   );
 }
 
 const CardWrapper = styled.div`
-  border: 3px solid darkblue;
-  border-radius: 5px;
+  height: 100%;
   background-color: hsl(210deg, 100%, 90%);
   padding: 10px;
   text-align: center;
   overflow: auto;
+`;
+
+const BigCardWrapper = styled.div`
+  overflow: hidden;
+  border: 3px solid darkblue;
+  border-radius: 15px;
 `;
 
 export default Card;
